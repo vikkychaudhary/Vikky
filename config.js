a50228e50277a7ee4f45f989188d6a5115262639
vikkychaudhary <chaudharyvikky07@gmail.com>
2024-06-21 10:21:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "vik1.jpg",
+    min2: "vik2.jpg",
+    min3: "vik3.jpg",
+    min4: "vik4.jpg",
+    min5: "vik5.jpg",
+    min6: "vik6.jpg",
+
+    max1: "vik7.jpg",
+    max2: "vik8.jpg",
+    max3: "vik9.jpg",
+    max4: "vik10.jpg",
+    max5: "vik11.jpg",
+    max6: "vik12.jpg",
+}
